84d182c3c9d014c68c1edd3e03fa65c45ae16f28
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-10 01:28:01 +0200

    Create Abstraction Project

---
+++ Abstractions/ILoggerManager.cs
@@ -0,0 +1,9 @@
+﻿namespace Abstractions;
+
+public interface ILoggerManager
+{
+    void LogInfo(string message);
+    void LogWarning(string message);
+    void LogDebug(string message);
+    void LogError(string message);
+}
