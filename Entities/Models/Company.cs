33fc0f437c8a55a9147ae978437622f704dfee0c
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-11 16:33:39 +0200

    Create database models

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Company
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string Country { get; set; }
+    public IEnumerable<Employee> Employees { get; set; }
+}
---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Employee
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Position { get; set; }
+    public Guid CompanyId { get; set; }
+    public Company Company { get; set; }
+
+}
