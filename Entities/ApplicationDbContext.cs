1615081c7c4e06201760b88a4552c18cf571cdfa
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-11 16:34:48 +0200

    Create DbContext for application

---
+++ Entities/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Entities;
+
+using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+}
