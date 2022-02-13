62502c6d98f2a0c67cfcc6c63bfba4d8ce4e6373
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-13 02:20:58 +0200

    Create MappinProfile

---
+++ ApiTraining/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiTraining;
+
+using AutoMapper;
+using Entities.Models;
+using Entities.ViewModels;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<Company, CompanyViewModel>()
+            .ForMember(e => e.FullAddress, 
+                option => option.MapFrom(x => string.Join(", ", x.Address, x.Country)));
+    }
+}
