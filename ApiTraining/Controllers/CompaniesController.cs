0a4b8779a981c7d4759656a829c1008ccde81cee
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-13 02:22:02 +0200

    Change ConnectionString and Add CompanyConteroller

---
+++ ApiTraining/Controllers/CompaniesController.cs
@@ -0,0 +1,40 @@
+﻿namespace ApiTraining.Controllers;
+
+using Abstractions;
+using AutoMapper;
+using Entities.Models;
+using Entities.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CompaniesController : ControllerBase
+{
+    private readonly IRepositoryManager _repository;
+    private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
+
+    public CompaniesController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+    {
+        this._repository = repository;
+        this._logger = logger;
+        this._mapper = mapper;
+    }
+
+    [HttpGet]
+    public IActionResult GetCompanies()
+    {
+        try
+        {
+            IEnumerable<CompanyViewModel> companies = _mapper.Map<IEnumerable<CompanyViewModel>>(_repository.Company.GetCompanies());
+            
+            return Ok(companies);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            return StatusCode(500, "Internal Server Error");
+        }
+    }
+}
