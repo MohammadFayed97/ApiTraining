8c656a4b3154a6fd2eac00b1fd2b43a890e0023a
Mohammad Fayed <mohammadfayed97@gmail.com>
2022-02-14 14:20:34 +0200

    Change message for Global Error Handling

--- ApiTraining/Extensions/ExceptionMiddlewareExtensions.cs
+++ ApiTraining/Extensions/ExceptionMiddlewareExtensions.cs
@@ -24,7 +24,7 @@
                         await context.Response.WriteAsync(new ErrorDetails
                         {
                             StatusCode = context.Response.StatusCode,
-                            Message = "Internal Server Error"
+                            Message = contextFeature.Error.Message
                         }.ToString());
                     }
                 });
